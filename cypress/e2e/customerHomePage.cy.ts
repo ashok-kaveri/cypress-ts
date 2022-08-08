1b34aed23aa3cd6b3be03a294ea41438637405f9
Ashok Kumar N <ashokkumarn@Ashoks-MacBook-Pro.local>
2022-08-08 11:26:25 +0530

    Add customer home page

--- cypress/e2e/customerHomePage.cy.ts
+++ cypress/e2e/customerHomePage.cy.ts
@@ -10,7 +10,7 @@ describe('Customer', () => {
         Landingpage.customerLoginButton();
         Customerpage.selectcustomername('Ron Weasly');
         Customerpage.clickLogin();
-        Customerpage.verifyCustomerNameOnCustomerLandingPage('Ron Weasly');
         Customerpage.verifyButtonsOnCustomerLoginLandingPage();
+        Customerpage.verifyCustomerNameOnCustomerLandingPage('Ron Weasly');
     })
 })
